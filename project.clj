ce4ded34d56f44c6bddc5208367079ac4e1cd050
Ram Krishnan <kriyative@gmail.com>
2013-07-09 17:36:52 -0700

    Release 1.1.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject org.clojars.kriyative/clj-http-client "1.1.0"
+(defproject org.clojars.kriyative/clj-http-client "1.1.1"
   :description "A minimal Clojure HTTP client"
-  :plugins [[lein-swank "1.4.4"]
-            [lein-clojars "0.9.1"]]
+  :plugins [[lein-swank "1.4.4"]]
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.4.0"]])
